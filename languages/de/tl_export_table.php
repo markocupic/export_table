1bbad7a650307aadff9b6fe3b53c68bcdfb6af47
Marko Cupic <m.cupic@gmx.ch>
2016-03-12 12:07:19 +0100

    dev

--- languages/de/tl_export_table.php
+++ languages/de/tl_export_table.php
@@ -7,9 +7,8 @@
  * @link https://github.com/markocupic/export_table
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  */
-
 $GLOBALS['TL_LANG']['tl_export_table']['settings'] = "Einstellungen";
-$GLOBALS['TL_LANG']['tl_export_table']['import_table']['0'] = "Datentabelle für Export auswählen";
+$GLOBALS['TL_LANG']['tl_export_table']['export_table']['0'] = "Datentabelle für Export auswählen";
 $GLOBALS['TL_LANG']['tl_export_table']['export_table']['1'] = "Wählen Sie eine Tabelle für den Exportvorgang aus.";
 $GLOBALS['TL_LANG']['tl_export_table']['selected_fields']['0'] = "Felder für Exportvorgang auswählen.";
 $GLOBALS['TL_LANG']['tl_export_table']['new']['0'] = "Neuen Exportdatensatz anlegen";
