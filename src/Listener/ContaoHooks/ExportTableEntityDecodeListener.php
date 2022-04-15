82c347039db98e40e7c554ff41559e53bd5fc885
Marko Cupic <m.cupic@gmx.ch>
2022-04-15 20:42:25 +0200

    decode html entites on export
    https://github.com/markocupic/export_table/pull/7

---
+++ src/Listener/ContaoHooks/ExportTableEntityDecodeListener.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Export Table.
+ *
+ * (c) Marko Cupic 2022 <m.cupic@gmx.ch>
+ * @license GPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/markocupic/export_table
+ */
+
+namespace Markocupic\ExportTable\Listener\ContaoHooks;
+
+use Contao\CoreBundle\ServiceAnnotation\Hook;
+use Markocupic\ExportTable\Config\Config;
+
+/**
+ * @Hook(ExportTableEntityDecodeListener::HOOK, priority=ExportTableEntityDecodeListener::PRIORITY)
+ */
+class ExportTableEntityDecodeListener implements ListenerInterface
+{
+    public const HOOK = 'exportTable';
+    public const PRIORITY = 500;
+
+    private static bool $disableHook = false;
+
+    /**
+     * @param string $strFieldname
+     * @param $varValue
+     * @param string $strTablename
+     * @param array $arrDataRecord
+     * @param array $arrDca
+     * @param Config $objConfig
+     * @return mixed|string
+     */
+    public function __invoke(string $strFieldname, $varValue, string $strTablename, array $arrDataRecord, array $arrDca, Config $objConfig)
+    {
+        if (static::$disableHook) {
+            return $varValue;
+        }
+
+        if (\is_string($varValue) && !empty($varValue)) {
+            $varValue = html_entity_decode($varValue, ENT_QUOTES);
+        }
+
+        return $varValue;
+    }
+
+    public static function disableHook(): void
+    {
+        self::$disableHook = true;
+    }
+
+    public static function enableHook(): void
+    {
+        self::$disableHook = false;
+    }
+
+    public static function isEnabled(): bool
+    {
+        return self::$disableHook;
+    }
+}
