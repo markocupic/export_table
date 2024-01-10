02e0a6dc474cbff9719975944597c14340c7fe96
Marko Cupic <marko.cupic@schule-ettiswil.ch>
2024-01-10 20:31:22 +0100

    PHP Doc update

--- .ecs/config/set/header_comment_fixer.php
+++ .ecs/config/set/header_comment_fixer.php
@@ -12,7 +12,7 @@ return static function (ECSConfig $ECSConfig): void {
     $services
         ->set(HeaderCommentFixer::class)
         ->call('configure', [[
-            'header' => "This file is part of Contao Export Table.\n\n(c) Marko Cupic 2023 <m.cupic@gmx.ch>\n@license GPL-3.0-or-later\nFor the full copyright and license information,\nplease view the LICENSE file that was distributed with this source code.\n@link https://github.com/markocupic/export_table",
+            'header' => "This file is part of Contao Export Table.\n\n(c) Marko Cupic 2024 <m.cupic@gmx.ch>\n@license GPL-3.0-or-later\nFor the full copyright and license information,\nplease view the LICENSE file that was distributed with this source code.\n@link https://github.com/markocupic/export_table",
         ]])
     ;
 };
